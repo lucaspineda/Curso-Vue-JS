ccadfedd90ca14a43070c59911b655f4b8eec196
Lucas <lucas.pineda@hotmail.com>
2019-05-29 08:47:07 -0300

    Desafio 3

---
+++ desafio-03/app.js
@@ -0,0 +1,17 @@
+new Vue({
+    el: '#desafio',
+    data: {
+        valor: 0
+    },
+    computed: {
+        resultado() {
+            return this.valor === 37 ?
+                'Valor igual 37' : 'Valor Diferente'
+        }
+    },
+    watch: {
+        resultado(novo,antigo){
+            setTimeout(() => { this.valor = 0},5000)
+        }
+    }
+});
