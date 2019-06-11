26ae189beb68f56e071cb053fb9ee554cdc7373e
lucaspc <lucas.pineda@hotmail.com>
2019-06-11 17:40:13 -0300

    Implement Special attack

--- Projeto-matador-de-monstros/app.js
+++ Projeto-matador-de-monstros/app.js
@@ -16,6 +16,7 @@ new Vue({
 		giveUp(){
 			this.started = false;
 			this.win = false;
+			this.vidaJogador = 0;
 		},
 		startGame(){
 			this.started = !this.started;
@@ -34,19 +35,29 @@ new Vue({
 			this.checkIfPlayerIsDead();
 			this.checkIfMonsterIsDead();
 		},
+		specialAttack(){
+			let valorJogador, valorMonstro = 0
+
+			valorJogador = 7
+			valorMonstro = 18
+
+			this.vidaJogador = this.vidaJogador - valorJogador;
+			this.vidaMonstro = this.vidaMonstro - valorMonstro;
+			this.checkIfPlayerIsDead();
+			this.checkIfMonsterIsDead();
+		},
 		checkIfPlayerIsDead(){
 			if(this.vidaJogador <= 0){
 				this.win = false;
 				this.started = false;
 				this.vidaJogador = 0;
-				console.log(this.vidaJogador)
 			}
 		},
 		checkIfMonsterIsDead(){
 			if(this.vidaMonstro <=0 ){
-				// width2 = '0';
 				this.win = true;
 				this.started = false;
+				this.vidaMonstro = 0;
 			}
 		}
 	}
