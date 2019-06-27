1c17ebfea2e84a535f2432347233c38c08a9c8bc
lucaspc <lucas.campos@me.com.br>
2019-06-27 20:11:33 -0300

    implement heal

--- Projeto-matador-de-monstros/app.js
+++ Projeto-matador-de-monstros/app.js
@@ -3,7 +3,8 @@ new Vue({
 	data: {
 		started: false,
 		playerLife: 100,
-		monsterLife: 100
+		monsterLife: 100,
+		logs: []
 	},
 	methods: {
 		startGame() {
@@ -12,13 +13,14 @@ new Vue({
 			this.monsterLife = 100;
 		},
 		attack(special) {
-			this.hurt('playerLife', 7,12, false);
-			this.hurt('monsterLife', 5,10, special)
+			this.hurt('playerLife', 7,12, false, 'Monstro', 'Jogador', 'monster');
+			this.hurt('monsterLife', 5,10, special, 'Jogador', 'Monstro', 'player')
 		},
-		hurt(atr, min, max, especial) {
+		hurt(atr, min, max, especial, source, target, cls) {
 			const plus = especial ? 5 : 0;
 			const hurt = this.getRandom(min + plus, max + plus);
 			this[atr] = Math.max (this[atr] - hurt, 0)
+			this.registrarLogs(`${source} atingiu ${target}`, cls)
 		},
 		getRandom(min, max){
 			const random = Math.random() * (max - min) + min
@@ -26,12 +28,14 @@ new Vue({
 		},
 		healAndHurt(){
 			this.hurt('playerLife', 0,1, false);
-
 			this.heal(5,10)
 		},
 		heal(min, max){
 			const heal = this.getRandom(min, max);
 			this.playerLife = Math.min(this.playerLife + heal, 100)
+		},
+		registrarLogs(text, cls){
+			this.logs.unshift( {text, cls} )
 		}
 	},
 	computed: {
@@ -45,5 +49,3 @@ new Vue({
 		}
 	}
 });
-
-// a
