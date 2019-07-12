779273bd3160c4b1377f579f69a7299fb879b87b
Lucas <lucas.pineda@hotmail.com>
2019-07-12 07:08:59 -0300

    Chamando componentes globais

--- componentes/componente-intro/src/App.vue
+++ componentes/componente-intro/src/App.vue
@@ -1,17 +1,15 @@
 <template>
 <div class="app">
     <h1>Contadores:</h1>
-    {{ contador }}
+    <app-contador />
+    <app-contador />
+    <app-contador />
 </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            contador: 0
-        }
-    }
+
 }
 </script>
 
---
+++ componentes/componente-intro/src/Contador.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="app">
+    <span>{{ contador }}</span>
+    <button @click="adicionar">+</button>
+    <button @click="adicionar">+</button>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            contador: 0
+        }
+    },
+    methods: {
+        adicionar() {
+            this.contador++
+        },
+        subtrair() {
+            this.contador--
+        }
+
+    }
+
+}
+</script>
+
+<style>
+
+</style>
