a61670faf5c1627fd840fab002d3ae6ae94d5aa8
Lucas <lucas.pineda@hotmail.com>
2019-07-15 08:41:26 -0300

    Deafio de organização de componentes com vue

---
+++ componentes/componente-desafio/src/Carousel.vue
@@ -0,0 +1,30 @@
+<template>
+<v-content>
+    <v-carousel>
+        <v-carousel-item
+            v-for="(item,i) in fotos"
+            :key="i"
+            :src="item.src"></v-carousel-item>
+    </v-carousel>
+</v-content>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            fotos: [
+				{src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'},
+				{src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'},
+				{src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'},
+				{src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'}
+			]
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
--- componentes/componente-desafio/src/main.js
+++ componentes/componente-desafio/src/main.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
+// importando a nivel global
 import Menu from './Menu.vue'
+// import Header from './Header.vue'
+import Carousel from './Carousel.vue'
+import Footer from './Footer.vue'
+
+
 
 Vue.config.productionTip = false,
 Vue.component('app-menu', Menu)
+// Vue.component('app-header', Header)
+Vue.component('app-carousel', Carousel)
+Vue.component('app-footer', Footer)
 
 
 new Vue({
