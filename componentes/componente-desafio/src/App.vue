f98aaa7bc71eb6ede656ff487d047a007e98b86b
Lucas <lucas.pineda@hotmail.com>
2019-07-15 08:52:17 -0300

    Aula sobre organização de pastas

--- componentes/componente-desafio/src/App.vue
+++ componentes/componente-desafio/src/App.vue
@@ -9,11 +9,17 @@
 
 <script>
 // importanto no escopo de componente
-import Header from './Header.vue'
+import Header from './components/template/Header.vue'
+import Menu from './components/template/Menu.vue'
+import Carousel from './components/widgets/Carousel.vue'
+import Footer from './components/template/Footer.vue'
 
 export default {
 	components: {
-		'app-header': Header
+		'app-header': Header,
+		'app-menu': Menu,
+		'app-carousel': Carousel,
+		'app-footer': Footer
 	}
 
 }
--- componentes/componente-desafio/src/main.js
+++ componentes/componente-desafio/src/main.js
@@ -2,19 +2,8 @@ import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
 // importando a nivel global
-import Menu from './Menu.vue'
-// import Header from './Header.vue'
-import Carousel from './Carousel.vue'
-import Footer from './Footer.vue'
-
-
 
 Vue.config.productionTip = false,
-Vue.component('app-menu', Menu)
-// Vue.component('app-header', Header)
-Vue.component('app-carousel', Carousel)
-Vue.component('app-footer', Footer)
-
 
 new Vue({
   render: h => h(App),
