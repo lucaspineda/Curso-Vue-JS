c7ae1010bab18dfd306afeb8755f63ba2bc9fcaa
lucaspc <lucas.campos@me.com.br>
2019-08-06 21:42:48 -0300

    criar projeto de mixins e filtros

---
+++ filtros-mixins/filtros-mixins-exercicios/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+	<div id="app">
+		<h1>Filtros & Mixins</h1>
+		<hr>
+		<p>432694</p>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			cpf: '43269499884'
+		}
+	},
+
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 2.5rem;
+}
+</style>
