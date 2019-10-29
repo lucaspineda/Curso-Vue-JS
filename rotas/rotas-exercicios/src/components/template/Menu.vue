5b0d08109df57f93ff4af84bd6974035fab7783b
lucaspc <lucas.campos@me.com.br>
2019-10-29 07:39:20 -0300

    Estilizar link do router

--- rotas/rotas-exercicios/src/components/template/Menu.vue
+++ rotas/rotas-exercicios/src/components/template/Menu.vue
@@ -1,8 +1,12 @@
 <template>
-  <nav class="menu">
-    <router-link to="/">Início</router-link>
-    <router-link to="/usuario">Usuários</router-link>
-  </nav>
+    <nav class="menu">
+        <ul>
+            <router-link to="/" tag="li" active-class="active" exact>
+                <a>Início</a></router-link>
+            <router-link to="/usuario" tag="li" active-class="active">
+                <a>Usuários</a></router-link>
+        </ul>
+    </nav>
 </template>
 
 <script>
