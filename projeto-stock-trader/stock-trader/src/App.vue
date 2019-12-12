4e25efaac1c0763837b80d1e5b74b054c941f27c
Lucas <lucas.pineda@hotmail.com>
2019-12-12 09:10:09 -0300

    startando projeto stock trader

---
+++ projeto-stock-trader/stock-trader/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <Header />
+</template>
+
+<script>
+import Header from './components/Header';
+
+export default {
+  name: 'App',
+
+  components: {
+    Header,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ projeto-stock-trader/stock-trader/src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-toolbar>
+        <v-toolbar-title>
+            <span>STOCK</span>
+            <span>TRADER</span>
+        </v-toolbar-title>
+        <v-toolbar-items>
+            <v-btn text>INÍCIO</v-btn>
+            <v-btn text>PORTFÓLIO</v-btn>
+            <v-btn text>AÇÕES</v-btn>
+        </v-toolbar-items>
+        <v-spacer></v-spacer>
+        <v-toolbar-items>
+            <v-btn text>FINALIZAR DIA</v-btn>
+            <v-btn text>SALVAR E CARREGAR</v-btn>
+            <v-btn text>SALDO:</v-btn>
+        </v-toolbar-items>
+    </v-toolbar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
